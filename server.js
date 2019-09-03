7a8a889a6e792f6e195ae00be46089776052de5f
Haixin Shi <hshi83@student.monash.edu>
2019-09-03 19:29:14 +1000

    week6

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 let express = require('express');
 let app = express();
 
+let mongodb = require('mongodb');
+let mongoClient = mongodb.MongoClient;
+let url = 'mongodb://localhost:27017';
+let col = null;
+mongoClient.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client){
+    if(err){
+        console.log('err: ', err);
+    }else{
+        db = client.db('week6lab');
+        col = db.collection('tasks');
+    }
+});
+
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
@@ -12,7 +25,7 @@ app.use(express.static('css'));
 
 var filePath = __dirname + "/views/";
 
-var db = [];
+// var db = [];
 
 
 app.get("/", function(req,res){
@@ -25,14 +38,105 @@ app.get("/newtask", function(req,res){
     res.sendFile(fileName);
 });
 
+app.get("/deleteone", function(req,res){
+    let fileName = filePath + "deleteone.html";
+    res.sendFile(fileName);
+});
+
+app.get("/deleteall", function(req,res){
+    let fileName = filePath + "deleteall.html";
+    res.sendFile(fileName);
+});
+
+app.get("/updatestate", function(req,res){
+    let fileName = filePath + "updatestate.html";
+    res.sendFile(fileName);
+});
+
+app.get("/insertmany", function(req,res){
+    let fileName = filePath + "insertmany.html";
+    res.sendFile(fileName);
+});
+
 app.get("/listtasks", function(req,res){
-    res.render("listtasks",{mydata: db});
+    // res.render("listtasks",{mydata: db});
+    col.find({}).toArray(function(err, result){
+        if(err){
+            console.log('err: ', err);
+        }else{
+            res.render("listtasks", {mydata: result});
+        }
+    });
 });
 
 app.post("/createtask", function(req,res){
     let data = req.body;
-    db.push(data);
-    res.render("listtasks",{mydata: db});
+    // db.push(data);
+    // res.render("listtasks",{mydata: db});
+    col.insertOne(data, function(err, result){
+        if(err){
+            console.log('err: ', err);
+        }else{
+            res.redirect('listtasks');
+        }
+    });
+});
+
+app.post("/createmany", function(req,res){
+    let data = req.body;
+    let mydata = [];
+    for(let i=0;i<data.count;i++){
+        mydata.push({
+            taskname: data.taskname,
+            ownername: data.ownername,
+            taskdue: data.taskdue,
+            taskstate: data.taskstate,
+            taskdesc: data.taskdesc
+        });
+
+    };
+
+    col.insertMany(mydata, function(err, result){
+        if(err){
+            console.log('err: ', err);
+        }else{
+            res.redirect('listtasks');
+        }
+    });
+});
+
+app.post("/deleteOne", function(req, res){
+    let __id = req.body.id;
+    let id = new mongodb.ObjectID(__id);
+    col.deleteOne({_id: id}, function(err, result){
+        if(err){
+            console.log('err: ', err);
+        }else{
+            res.redirect('listtasks')
+        }
+    });
+});
+
+app.post("/deleteAll", function(req, res){
+    col.deleteMany({}, function(err, result){
+        if(err){
+            console.log('err: ', err);
+        }else{
+            res.redirect('listtasks');
+        }
+    });
+});
+
+app.post('/update', function(req, res){
+    let id = req.body.id;
+    let _taskstate = req.body.taskstate;
+    col.updateOne({_id:id}, {$set:{taskstate:_taskstate}}, (err, result)=>{
+        if(err){
+            console.log('err: ', err);
+        }else{
+            res.redirect('listtasks');
+        }
+    });
 });
 
 app.listen(8080, ()=>{
